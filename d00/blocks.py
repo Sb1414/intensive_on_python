4afa4d93f0881d3a3e41ce32241bb18f0e430a19
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-08 23:28:14 +0300

    d00 ex00 -func

--- d00/blocks.py
+++ d00/blocks.py
@@ -1,15 +1,11 @@
 import sys
 
-def filter_lines(lines, count):
-    filtered_lines = []
-    for line in lines[:count]:
-        line = line.strip()
-        if len(line) == 32 and line.startswith('00000') and not line.startswith('000000'):
-            filtered_lines.append(line)
-    return filtered_lines
-
 count = int(sys.argv[1])
 lines = sys.stdin.readlines()
-filtered_lines = filter_lines(lines, count)
+filtered_lines = []
+for line in lines[:count]:
+    line = line.strip()
+    if len(line) == 32 and line.startswith('00000') and not line.startswith('000000'):
+        filtered_lines.append(line)
 for line in filtered_lines:
     print(line)
