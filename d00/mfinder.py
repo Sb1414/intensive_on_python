3215a1c2932c705253fc6621ca67efa7a63bb617
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-09 18:17:12 +0300

    проверка на количество символов

--- d00/mfinder.py
+++ d00/mfinder.py
@@ -2,4 +2,8 @@ with open('m.txt', 'r') as file:
     content = file.readlines()
 
 array = [line.strip() for line in content]
-print(array)
+
+if len(array) != 3 or any(len(line) != 5 for line in array):
+    print("Error")
+else:
+    print("True")
