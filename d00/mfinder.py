57c744a3ee0513c15c7203e7f312cf5fff344f6e
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-09 18:31:21 +0300

    d00 ex02

--- d00/mfinder.py
+++ d00/mfinder.py
@@ -6,4 +6,17 @@ array = [line.strip() for line in content]
 if len(array) != 3 or any(len(line) != 5 for line in array):
     print("Error")
 else:
+    count = sum(line.count('*') for line in array)
+    if count != 9:
+        print("False")
+        exit()
+    if array[0][0] != '*' or array[0][4] != '*':
+        print("False")
+        exit()
+    if array[1][0] != '*' or array[1][1] != '*' or array[1][3] != '*' or array[1][4] != '*':
+        print("False")
+        exit()
+    if array[2][0] != '*' or array[2][2] != '*' or array[2][4] != '*':
+        print("False")
+        exit()
     print("True")
