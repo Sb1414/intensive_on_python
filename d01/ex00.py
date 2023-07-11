3f2f3290f7e1b5797d26323fdc69674e1581d5fd
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-11 13:40:13 +0300

    d01 ex00 + bonus

--- d01/ex00.py
+++ d01/ex00.py
@@ -22,3 +22,27 @@ purse = add_ingot(get_ingot(add_ingot(purse)))
 
 print(purse)
 
+def test_empty():
+    purse = {"gold_ingots": 5, "silver_coins": 10}
+    empty_purse = empty(purse)
+    assert empty_purse == {}, "Тест empty не пройден"
+    assert purse == {"gold_ingots": 5, "silver_coins": 10}, "Тест empty изменяет входной кошелек"
+
+def test_add_ingot():
+    purse = {"gold_ingots": 2}
+    new_purse = add_ingot(purse)
+    assert new_purse == {"gold_ingots": 3}, "Тест add_ingot не пройден"
+    assert purse == {"gold_ingots": 2}, "Тест add_ingot изменяет входной кошелек"
+
+def test_get_ingot():
+    purse = {"gold_ingots": 4}
+    new_purse = get_ingot(purse)
+    assert new_purse == {}, "Тест get_ingot не пройден"
+    assert purse == {"gold_ingots": 4}, "Тест get_ingot изменяет входной кошелек"
+
+if __name__ == "__main__":
+    test_empty()
+    test_add_ingot()
+    test_get_ingot()
+    print("Все тесты успешно пройдены")
+
