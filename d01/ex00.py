fb022bdf420c76b02128d0534e1e5552479a228c
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-11 17:05:03 +0300

    replace

--- d01/ex00.py
+++ d01/ex00.py
@@ -1,16 +1,16 @@
 from typing import Dict
 
-def empty(purse: Dict[str, int]) -> Dict[str, int]:
+def empty(purse: Dict[str, int]):
     return {}
 
-def add_ingot(purse: Dict[str, int]) -> Dict[str, int]:
+def add_ingot(purse: Dict[str, int]):
     new_purse = purse.copy()
 
     new_purse["gold_ingots"] = new_purse.get("gold_ingots", 0) + 1
 
     return new_purse
 
-def get_ingot(purse: Dict[str, int]) -> Dict[str, int]:
+def get_ingot(purse: Dict[str, int]):
     new_purse = purse.copy()
 
     new_purse.pop("gold_ingots", None)
@@ -25,24 +25,24 @@ print(purse)
 def test_empty():
     purse = {"gold_ingots": 5, "silver_coins": 10}
     empty_purse = empty(purse)
-    assert empty_purse == {}, "Тест empty не пройден"
-    assert purse == {"gold_ingots": 5, "silver_coins": 10}, "Тест empty изменяет входной кошелек"
+    assert empty_purse == {}, "false"
+    assert purse == {"gold_ingots": 5, "silver_coins": 10}, "false"
 
 def test_add_ingot():
     purse = {"gold_ingots": 2}
     new_purse = add_ingot(purse)
-    assert new_purse == {"gold_ingots": 3}, "Тест add_ingot не пройден"
-    assert purse == {"gold_ingots": 2}, "Тест add_ingot изменяет входной кошелек"
+    assert new_purse == {"gold_ingots": 3}, "false"
+    assert purse == {"gold_ingots": 2}, "false"
 
 def test_get_ingot():
     purse = {"gold_ingots": 4}
     new_purse = get_ingot(purse)
-    assert new_purse == {}, "Тест get_ingot не пройден"
-    assert purse == {"gold_ingots": 4}, "Тест get_ingot изменяет входной кошелек"
+    assert new_purse == {}, "false"
+    assert purse == {"gold_ingots": 4}, "false"
 
 if __name__ == "__main__":
     test_empty()
     test_add_ingot()
     test_get_ingot()
-    print("Все тесты успешно пройдены")
+    print("Все тесты пройдены")
 
