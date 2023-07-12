7cd341d3d1d329802fed7634414ff75f4e2f1054
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-12 16:32:11 +0300

    task ex01 has been redone

--- d00/decypher.py
+++ d00/decypher.py
@@ -1,16 +1,9 @@
 import sys
 
-def split_and_swap(string):
-    words = string.split()
-    length = len(words)
-    part_length = length // 4
-    part1 = words[:part_length]
-    part2 = words[-part_length:]
-    part3 = words[part_length:part_length*2]
-    part4 = words[part_length*2:-part_length]
-    result = ''.join(part1 + part4 + part3 + part2)
-    return result
+def decipher_words(args):
+    words = [word[0] for word in args.split() if word.isalpha()]
+    return ''.join(words)
 
 input_string = ' '.join(sys.argv[1:])
-result_string = split_and_swap(input_string)
+result_string = decipher_words(input_string)
 print(result_string)
