d4eab42c6292f01b285e8a629e004505af311115
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-15 18:53:32 +0300

    ex02 + color

--- d04/pressure.py
+++ d04/pressure.py
@@ -1,30 +1,37 @@
 import random
 import time
+from colorama import Fore, Style
 
 def emit_gel(pressure, step):
     while True:
         pressure += random.uniform(0, step)
         if pressure > 100:
-            raise ValueError(f"Pressure too high: {pressure}")
+            print(f"{Fore.RED}Pressure too high: {round(pressure)}{Style.RESET_ALL}")
         yield pressure
 
 def valve():
     step_sign = 20  # шаг
-    pressure = 75  # начальное давление
+    pressure = 50  # начальное давление
     generator = emit_gel(pressure, step_sign)  # значение шага
     while True:
         try:
             pressure = next(generator)
             if pressure < 10:
-                print(f"Pressure too low: {round(pressure)}")
+                print(f"{Fore.RED}Pressure too low: {round(pressure)}{Style.RESET_ALL}")
                 break
             elif pressure > 90:
-                print(f"Pressure too high: {round(pressure)}")
+                print(f"{Fore.RED}Pressure too high: {round(pressure)}{Style.RESET_ALL}")
                 break
-            elif pressure < 20 or pressure > 80:
-                step_sign *= -1  # изменение направления
+            elif pressure < 20:
+                step_sign = abs(step_sign)  # изменение направления на положительное
                 generator = emit_gel(pressure, step_sign)  # новый генератор с обновленным давлением и знаком остановки
-            print(f"Pressure: {round(pressure)}")
+                print(f"{Fore.YELLOW}Pressure: {round(pressure)}{Style.RESET_ALL}")
+            elif pressure > 80:
+                step_sign = -abs(step_sign)  # изменение направления на отрицательное
+                generator = emit_gel(pressure, step_sign)  # новый генератор с обновленным давлением и знаком остановки
+                print(f"{Fore.YELLOW}Pressure: {round(pressure)}{Style.RESET_ALL}")
+            else:
+                print(f"Pressure: {round(pressure)}")
             time.sleep(0.5)  # задержка между измерениями давления
         except ValueError as e:
             print(e)
